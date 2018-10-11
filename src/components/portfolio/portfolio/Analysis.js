686e88f22a3c3505fa233c0de49b2f1b8caf300c
reese820 <38567375+reese820@users.noreply.github.com>
2018-10-10 20:53:00 -0400

    Add files via upload

---
+++ src/components/portfolio/portfolio/Analysis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableRender from './TableRender';
+import ChartRender from './ChartRender';
+
+class Analysis extends React.Component {
+    render() {
+        <ChartRender/>,
+        <TableRender/>;
+    }
+}
+
+export default Analysis;
---
+++ src/components/portfolio/portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Analysis from './Analysis';
+import Build from './Build';
+
+class Portfolio extends React.Component {
+    render() {
+        <Build/>,
+        <Analysis/>;
+    }
+}
+
+ComponentOnMount();
+
+export default Portfolio;
